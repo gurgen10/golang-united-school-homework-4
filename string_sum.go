3274169ab57982bfde809c3c2650b7e818d2e391
Gurgen Torosyan <gurgen.torosyan@exio.tech>
2022-05-23 20:53:51 +0400

    Fixed Error msg

--- string_sum.go
+++ string_sum.go
@@ -39,26 +39,31 @@ func StringSum(input string) (output string, err error) {
 		input = input[1:]
 	}
 	
-	isAdding := strings.Contains(input, "+")
+	splitInputPlus := strings.Split(input, "+")
+	splitInputMinus := strings.Split(input, "-")
 
-	if isAdding {
-		val1, err1 := strconv.Atoi(string([]rune(input)[0]))
-		val2, err2 := strconv.Atoi(string([]rune(input)[2]))
-
-		if(err1 != nil || err2 != nil) {
-			printError(errorNotTwoOperands)
-			return "", nil
-		}
-		output = strconv.Itoa(val1 + val2)
-	} else {
-		val1, err1 := strconv.Atoi(string([]rune(input)[0]))
-		val2, err2 := strconv.Atoi(string([]rune(input)[2]))
+	if len(splitInputPlus) > 1 {
+		val1, err1 := strconv.ParseInt(splitInputPlus[0], 10, 32)
+		val2, err2 := strconv.ParseInt(splitInputPlus[1], 10, 32)
+		fmt.Println(int(val2))
 
 		if(err1 != nil || err2 != nil) {
 			printError(errorNotTwoOperands)
 			return "", nil
 		}
-		output = strconv.Itoa(val1 - val2)
+		output = fmt.Sprintf("%v",int64(val1) + int64(val2));
+		return output, nil
+	} 
+	if len(splitInputMinus) > 1 {
+		val1, err1 := strconv.ParseInt(splitInputMinus[0], 10, 32)
+		val2, err2 := strconv.ParseInt(splitInputMinus[1], 10, 32)
+
+		if(err1 != nil || err2 != nil) {
+			printError(errorNotTwoOperands)
+			return "", nil
+		}
+		output = fmt.Sprintf("%v",int64(val1) + int64(val2));
+		return output, nil
 	}
 
 	return output, nil
